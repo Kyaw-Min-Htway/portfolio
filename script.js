b81e016c7a0e79dc15f6b02ec46ccf55f318cf50
Kyaw Min Htway <batman066813@gmail.com>
2024-10-14 22:40:45 +0630

    project

--- script.js
+++ script.js
@@ -54,4 +54,23 @@ document.getElementById('downloadCvButton').addEventListener('click', function (
     link.click();
     document.body.removeChild(link);
   });
+
+  const circles = document.querySelectorAll('.circle');
+  circles.forEach(elem => {
+    var dots = elem.getAttribute("data-dots");
+    var marked = elem.getAttribute("data-percent");
+    var percent = Math.floor(dots*marked/100);
+    var points = "";
+    var rotate = 360 / dots;
+
+    for(let i = 0 ; i < dots ; i++){
+        points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+    }
+    elem.innerHTML = points;
+
+    const pointsMarked = elem.querySelectorAll('.points');
+    for(let i = 0; i < percent ; i++){
+        pointsMarked[i].classList.add('marked')
+    }
+  })
   
